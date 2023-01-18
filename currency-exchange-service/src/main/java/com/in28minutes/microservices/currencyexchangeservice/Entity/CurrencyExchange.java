dfd44cbc78747c1a42ae586ebe8c161df19836d6
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-01-18 09:27:25 -0500

    Modified pom.xml files to make sure that they support docker.

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/Entity/CurrencyExchange.java
@@ -2,9 +2,9 @@ package com.in28minutes.microservices.currencyexchangeservice.Entity;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class CurrencyExchange {
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,13 @@ public class CurrencyExchangeController {
 	private Environment environment;
 	@Autowired
 	private CurrenyExchangeReposetory currenyExchangeReposetory;
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
 //		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(80));
+		logger.info("retrieveExchangeValue called with {} to {}",from,to);
 		CurrencyExchange currencyExchange = currenyExchangeReposetory.findByFromAndTo(from, to);
 		if (currencyExchange == null)
 			throw new RuntimeException("unable to finddata from" + from + "to" + to);
